0da6b007439ee111d93299391b7569faa30cd2b6
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-12-14 23:33:34 +0000

    expose router

--- app/server/server.js
+++ app/server/server.js
@@ -1,3 +1,10 @@
 var barbakoa = require("barbakoa");
+var router = barbakoa.router;
+
 var app = new barbakoa();
+
+router.get("/hello", function* () {
+  this.body = {"msg": "hello world"};
+});
+
 app.start();
