88427a0930e52ddb40399948581d6510d412af01
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-12-24 12:52:10 +0000

    compile templates

--- app/client/js/Client.js
+++ app/client/js/Client.js
@@ -1,12 +1,32 @@
-class Client {
-  constructor (msg) {
-    this.msg = msg;
+var app = angular.module("app", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "templates/template1.html",
+      controller: "Template1Controller as ctrl"
+    })
+    .when("/2", {
+      templateUrl: "templates/template2.html"
+    })
+});
+
+class Template1Controller {
+
+  constructor(commonService) {
+    this.commonService = commonService;
   }
 
-  sayHello () {
-    console.log(this.msg + "abc");
+  sayHelloWorld() {
+    console.log("hello");
+    this.commonService.sayWorld();
   }
 }
+app.controller("Template1Controller", Template1Controller);
 
-var client = new Client("msg asdf ");
-client.sayHello();
+class CommonService {
+  sayWorld() {
+    console.log("world");
+  }
+}
+app.service("commonService", CommonService);
